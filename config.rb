f53c2bbbfa4aec8e4986430ddacdac789205dcae
Nadav Spiegelman <me@nadav.name>
2014-10-04 17:07:56 -0400

    Turn on directory indexes, clean up config.rb

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
 require 'modular-scale'
-#
+
 set :markdown_engine, :redcarpet
 set :markdown, :smartypants => true, :footnotes => true
-###
-# Compass
-###
+
 activate :autoprefixer do |config|
   config.browsers = ['last 2 versions', 'Explorer >= 9']
 end
+activate :directory_indexes
+
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
@@ -37,53 +37,14 @@ activate :blog do |blog|
   # blog.page_link = "page/{num}"
 end
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
-# Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
