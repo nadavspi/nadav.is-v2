6a8523806aed57013ff5a3bc6adb2521cb804644
Nadav Spiegelman <me@nadav.name>
2014-10-20 01:27:57 +0300

    Improve mobile nav JS

--- source/javascripts/zz-nadav.js
+++ source/javascripts/zz-nadav.js
@@ -2,12 +2,21 @@ window.onload = function() {
     fitVids('.fitvids');
 }
 
-$('button[aria-controls="navigation"]').on('click', function() {
-    $nav = $("#navigation");
+Zepto(function($) {
 
-    if ($nav.attr('aria-expanded') == 'false') {
-        $nav.attr('aria-expanded', true).focus();
-    } else {
-        $nav.attr('aria-expanded', false);
+    var $nav = $("#navigation");
+
+    $('button[aria-controls="navigation"]').on('click', function() {
+
+        if ($nav.attr('aria-expanded') == 'false') {
+            $nav.attr('aria-expanded', true).focus();
+        } else {
+            $nav.attr('aria-expanded', false);
+        }
+    });
+
+    if (window.outerWidth >= 800) {
+        $nav.attr('aria-expanded', true).attr('tabindex', 1);
     }
+
 });
