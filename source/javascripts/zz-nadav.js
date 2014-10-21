a5be9ad6c24a77e868a454d1cc2da21af29ef23c
Nadav Spiegelman <me@nadav.name>
2014-10-21 18:50:13 +0300

    Add expanded class to nav button

--- source/javascripts/zz-nadav.js
+++ source/javascripts/zz-nadav.js
@@ -7,6 +7,7 @@ Zepto(function($) {
     var $nav = $("#navigation");
 
     $('button[aria-controls="navigation"]').on('click', function() {
+        $(this).toggleClass('is-expanded');
 
         if ($nav.attr('aria-expanded') == 'false') {
             $nav.attr('aria-expanded', true).focus();
