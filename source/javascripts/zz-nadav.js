40efd642ad07d30358dcbb54f67fe38d80c0f52f
Nadav Spiegelman <me@nadav.name>
2015-07-01 22:55:07 -0400

    Restyle navigation

--- source/javascripts/zz-nadav.js
+++ source/javascripts/zz-nadav.js
@@ -1,23 +1,3 @@
 window.onload = function() {
     fitVids('.fitvids');
-}
-
-(function() {
-    var nav = document.getElementById('navigation');
-
-    document.querySelector('button[aria-controls="navigation"]')
-        .addEventListener('click', function(e) {
-            if (nav.getAttribute('aria-expanded') == 'false') {
-                nav.setAttribute('aria-expanded', true);
-                nav.focus();
-            } else {
-                nav.setAttribute('aria-expanded', false);
-            }
-        }, false);
-
-    if (window.outerWidth >= 800) {
-        nav.setAttribute('aria-expanded', true);
-        nav.setAttribute('tabindex', 1);
-    }
-
-}());
+};
