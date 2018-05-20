567ae38082dbae46ae8a709f818cb7d4931d0570
Nadav Spiegelman <me@nadav.name>
2018-05-19 20:29:22 -0400

    Add book template without image

---
+++ src/templates/book.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default ({ data }) => {
+  const post = data.markdownRemark;
+  return (
+    <section className="book">
+      <h1>{post.frontmatter.title}</h1>
+      <header className="book__header">
+        <dl>
+          <dt>Author</dt>
+          <dd>{post.frontmatter.author}</dd>
+
+          <dt>Year of Publication</dt>
+          <dd>{post.frontmatter.publication}</dd>
+
+          <dt>When I read it</dt>
+          <dd>{post.frontmatter.reading}</dd>
+
+          <dt>What I thought</dt>
+          <dd>{post.frontmatter.rating}</dd>
+
+          <dt>Buy the book</dt>
+          <dd><a href={post.frontmatter.link}>Amazon</a></dd>
+        </dl>
+      </header>
+
+      {post.frontmatter.highlights && <Highlights highlights={post.frontmatter.highlights} />}
+    </section>
+  )
+}
+
+const Highlights = ({ highlights }) => (
+  <article>
+    <h2>Choice Highlights</h2>
+    {highlights.map(highlight => (
+      <blockquote key={highlight.substring(0, 15)}>{highlight}</blockquote>
+    ))}
+  </article>
+);
+
+export const query = graphql`
+  query BookQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        author
+        publication
+        reading
+        rating
+        link
+        highlights {
+          id
+          highlight
+        }
+      }
+    }
+  }
+`
+
